954f5d275221606710f50f024e12292957811658
hwalborn <hwalborn@gmail.com>
2017-03-19 13:45:02 -0400

    Not too much work to do

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+
+  products.forEach(product => {
+    if (product.discount >= .5) {
+      totalPrice += product.price;
+    }
+  });
+
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products));
